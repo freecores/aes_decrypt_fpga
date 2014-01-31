14d27571736f4b4a7c3e433994e109bb7d61b509
schengopencores <schengopencores@0415bec1-0fdd-4b3b-9129-5eb6ac7caad0>
2014-01-31 16:01:36 +0000

    Standard Opencores file header added to aes_decrypt128_tb.sv, aes_decrypt192_tb.sv aes_decrypt256_tb.sv.

--- bench/verilog/aes_decrypt192_tb.sv
+++ bench/verilog/aes_decrypt192_tb.sv
@@ -1,3 +1,50 @@
+////////////////////////////////////////////////////////////////// ////
+//// 																////
+//// AES Decryption Core for FPGA									////
+//// 																////
+//// This file is part of the AES Decryption Core for FPGA project 	////
+//// http://www.opencores.org/cores/xxx/ 							////
+//// 																////
+//// Description 													////
+//// Implementation of  AES Decryption Core for FPGA according to 	////
+//// core specification document.		 							////
+//// 																////
+//// To Do: 														////
+//// - 																////
+//// 																////
+//// Author(s): 													////
+//// - scheng, schengopencores@opencores.org 						////
+//// 																////
+//////////////////////////////////////////////////////////////////////
+//// 																////
+//// Copyright (C) 2009 Authors and OPENCORES.ORG 					////
+//// 																////
+//// This source file may be used and distributed without 			////
+//// restriction provided that this copyright statement is not 		////
+//// removed from the file and that any derivative work contains 	////
+//// the original copyright notice and the associated disclaimer. 	////
+//// 																////
+//// This source file is free software; you can redistribute it 	////
+//// and/or modify it under the terms of the GNU Lesser General 	////
+//// Public License as published by the Free Software Foundation; 	////
+//// either version 2.1 of the License, or (at your option) any 	////
+//// later version. 												////
+//// 																////
+//// This source is distributed in the hope that it will be 		////
+//// useful, but WITHOUT ANY WARRANTY; without even the implied 	////
+//// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 		////
+//// PURPOSE. See the GNU Lesser General Public License for more 	////
+//// details. 														////
+//// 																////
+//// You should have received a copy of the GNU Lesser General 		////
+//// Public License along with this source; if not, download it 	////
+//// from http://www.opencores.org/lgpl.shtml 						////
+//// 																//// ///
+///////////////////////////////////////////////////////////////////
+////																////
+//// Testbench for 192-bit decryption								////
+////																////
+////////////////////////////////////////////////////////////////////////
 `timescale 1ns/1ps
 
 // Uncomment the following line if you're targetting Xilinx FPGA
